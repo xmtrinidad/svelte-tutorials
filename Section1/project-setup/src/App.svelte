f90dd9763e1e99205809680ad144a15f52da4368
xmtrinidad <xmtrinidad@gmail.com>
2020-11-27 11:23:25 -0700

    added function

--- Section1/project-setup/src/App.svelte
+++ Section1/project-setup/src/App.svelte
@@ -1,5 +1,11 @@
 <script>
 	export let name;
+	export let age;
+	const thing = 'This is a test thing';
+
+	function incrementAge() {
+		age += 1;
+	}
 </script>
 
 <style>
@@ -8,4 +14,6 @@
 	}
 </style>
 
-<h1>Hello {name}!</h1>
+<h1>Hello {name}!, my age is {age}</h1>
+<h2>{thing}</h2>
+<button on:click="{incrementAge}">Change Age</button>
